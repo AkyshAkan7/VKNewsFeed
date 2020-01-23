9a8943d325a98aae23c2e8097e0fe2902e524351
Akan <23879@iitu.kz>
2020-01-23 16:32:59 +0600

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Akan Akysh on 1/23/20.
+//  Copyright © 2020 Akysh Akan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
